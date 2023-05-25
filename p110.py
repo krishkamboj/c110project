ee1c36ba7daef44f05bd351e7dd9b3070fb81c25
krishkamboj <110669632+krishkamboj@users.noreply.github.com>
2023-05-25 09:46:40 +0530

    Add files via upload

---
+++ p110.py
@@ -0,0 +1,19 @@
+import random
+
+respones = "y"
+while respones == "y":
+    no = random.randint(1,6)
+    if no ==1:
+        print("The number on the dice is 1")
+    if no ==2:
+        print("The number on the dice is 2")
+    if no ==3:
+        print("The number on the dice is 3")
+    if no ==4:
+        print("The number on the dice is 4")
+    if no ==5:
+        print("The number on the dice is 5")
+    if no ==6:
+        print("The number on the dice is 6")
+    
+    respones=input("Press y to roll again")
